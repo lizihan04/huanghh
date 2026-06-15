dc3f88660b1905cc2d69ce09bebb9ed8f0d12819
lizihan04 <3354105731@qq.com>
2026-06-15 09:46:13 +0800

    item

--- src/entity/Item.java
+++ src/entity/Item.java
@@ -58,13 +58,36 @@ public abstract class Item {
 
 
     public abstract void use(Player player);//玩家使用
-    public void addCount(int num){
-        System.out.println("拾取"+getName()+num+"个");
+
+    public void addCount(int num) {
+        System.out.println("拾取" + getName() + num + "个,当前数量：" + getCount());
+        this.count += num;
     }
-    public  void reduceCount1(int num){
-        System.out.println("丢弃"+getName()+num+"个");
+
+    public void reduceCount1(int num) {
+        if (this.count <= 0) {
+            System.out.println("【" + getName() + "】数量为0，无需丢弃");
+        }
+        if (this.count > num) {
+            this.count -= num;
+            System.out.println("丢弃" + getName() + num + "个，当前数量：" + getCount());
+        } else {
+            System.out.println("物品数量不足，剩余" + getCount() + "个全部丢弃！当前数量为0");
+            this.count = 0;
+        }
     }//丢弃
-    public  void reduceCount2(int num){
-        System.out.println("使用"+getName()+num+"个");
-    }//使用
+
+    public void reduceCount2(int num) {
+        if (this.count <= 0) {
+            System.out.println("【" + getName() + "】数量为0，无法使用");
+        }
+        if (this.count >= num) {
+            this.count -= num;
+            System.out.println("使用【" + getName() + "】x" + num + "，剩余数量：" + getCount());
+        }//使用
+        else {
+            this.count = 0;
+            System.out.println("数量不足，已用完所有【" + getName() + "】x" + num + "，剩余数量：0");
+        }
+    }
 }
