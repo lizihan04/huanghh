401997d1df480ef3f86130b5f9bcdd768da36f68
haozhenCai <2707358954@qq.com>
2026-06-28 01:21:02 +0800

    new FileUtil

--- src/com/survivalgame/service/RecipeManagement.java
+++ src/com/survivalgame/service/RecipeManagement.java
@@ -3,17 +3,12 @@ package com.survivalgame.service;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * 合成配方数据类
- * 存储所有合成配方
- */
 public class RecipeManagement {
 
-    // 配方表：配方名 -> 材料名 -> 数量
     public static final Map<String, Map<String, Integer>> RECIPES = new HashMap<>();
 
     static {
-        // 贝刃：3贝壳 + 2藤蔓（用来开椰子）
+        // 贝刃：3贝壳 + 2藤蔓
         Map<String, Integer> shellBlade = new HashMap<>();
         shellBlade.put("贝壳", 3);
         shellBlade.put("藤蔓", 2);
@@ -23,17 +18,17 @@ public class RecipeManagement {
         Map<String, Integer> stoneBlade = new HashMap<>();
         stoneBlade.put("石头", 2);
         stoneBlade.put("藤蔓", 3);
-        RECIPES.put("石斧", stoneBlade);
+        RECIPES.put("石刃", stoneBlade);
 
-        // 木棒：3树枝 + 2藤蔓
+        // 木棒：3木头 + 2藤蔓
         Map<String, Integer> woodClub = new HashMap<>();
-        woodClub.put("树枝", 3);
+        woodClub.put("木头", 3);
         woodClub.put("藤蔓", 2);
         RECIPES.put("木棒", woodClub);
 
-        // 锤子：1石头 + 1藤蔓
+        // 锤子：2石头 + 1藤蔓
         Map<String, Integer> hammer = new HashMap<>();
-        hammer.put("石头", 1);
+        hammer.put("石头", 2);
         hammer.put("藤蔓", 1);
         RECIPES.put("锤子", hammer);
 
