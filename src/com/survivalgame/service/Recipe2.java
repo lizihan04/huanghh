93fb3449b5a4d2cef5904d275da6688387ed447f
”Jiaxin2727“ <1006889290@qq.com>
2026-06-29 00:35:31 +0800

    Recipe update

--- src/com/survivalgame/service/Recipe2.java
+++ src/com/survivalgame/service/Recipe2.java
@@ -1,5 +1,65 @@
 package com.survivalgame.service;
 
+import entity.Item2;
+import entity.Player2;
+
 public class Recipe2 {
+    Player2 player = Player2.getInstance();
+    Item2[] backpackArr = player.getBackpackArr();
+
+    //制作木棒
+    public void woodStick(){
+        if(backpackArr[7].getOwnCount()>=2 && backpackArr[8].getOwnCount()>=3){
+            backpackArr[7].setOwnCount(backpackArr[7].getOwnCount()-2);
+            backpackArr[8].setOwnCount(backpackArr[8].getOwnCount()-3);
+            backpackArr[13].setOwnCount(backpackArr[9].getOwnCount()+1);
+        }else{
+            System.out.println("材料不足");
+        }
+    }
+
+    //制作贝刃
+    public void beiRen(){
+        if(backpackArr[13].getOwnCount()>=1&&backpackArr[5].getOwnCount()>=3){
+            backpackArr[13].setOwnCount(backpackArr[13].getOwnCount()-1);
+            backpackArr[5].setOwnCount(backpackArr[5].getOwnCount()-3);
+            backpackArr[11].setOwnCount(backpackArr[11].getOwnCount()+1);
+        }else{
+            System.out.println("材料不足");
+        }
+    }
+
+    //制作石剑
+    public void stoneSword(){
+        if(backpackArr[13].getOwnCount()>=2&&backpackArr[4].getOwnCount()>=5){
+            backpackArr[13].setOwnCount(backpackArr[13].getOwnCount()-2);
+            backpackArr[4].setOwnCount(backpackArr[4].getOwnCount()-5);
+            backpackArr[14].setOwnCount(backpackArr[14].getOwnCount()+1);
+        }else{
+            System.out.println("材料不足");
+        }
+    }
+
+    //制作斧头
+    public void axe(){
+        if(backpackArr[13].getOwnCount()>=1&&backpackArr[4].getOwnCount()>=3){
+            backpackArr[13].setOwnCount(backpackArr[13].getOwnCount()-1);
+            backpackArr[4].setOwnCount(backpackArr[8].getOwnCount()-3);
+            backpackArr[10].setOwnCount(backpackArr[10].getOwnCount()+1);
+        }else{
+            System.out.println("材料不足");
+        }
+    }
+
+    //制作锤子
+    public void hammer(){
+        if(backpackArr[13].getOwnCount()>=1&&backpackArr[4].getOwnCount()>=3){
+            backpackArr[13].setOwnCount(backpackArr[13].getOwnCount()-1);
+            backpackArr[4].setOwnCount(backpackArr[4].getOwnCount()-3);
+            backpackArr[12].setOwnCount(backpackArr[12].getOwnCount()+1);
+        }else{
+            System.out.println("材料不足");
+        }
+    }
 
 }
