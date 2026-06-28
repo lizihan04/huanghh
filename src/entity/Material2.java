801cce0a8338e11cd263406f33d91150accb6c47
”Jiaxin2727“ <1006889290@qq.com>
2026-06-28 22:54:13 +0800

    Item update

--- src/entity/Material2.java
+++ src/entity/Material2.java
@@ -1,5 +1,11 @@
 package entity;
 
 public class Material2 extends Item2{
-    //名称＋数量
+    //构造方法
+    public Material2() {
+    }
+
+    public Material2(String itemName, int ownCount) {
+        super(itemName, ownCount);
+    }
 }
--- src/entity/Tool2.java
+++ src/entity/Tool2.java
@@ -1,6 +1,25 @@
 package entity;
 
-public class Tool2 extends Item2{
+public class Tool2 extends Item2 {
     // 工具专属（无采集加成）
-    private int attackBonus;       // 攻击加成
+    private int attackBonus;// 攻击加成
+
+    //构造方法
+    public Tool2() {
+    }
+
+    public Tool2(String itemName, int ownCount, int attackBonus) {
+        super(itemName, ownCount);
+        this.attackBonus = attackBonus;
+    }
+
+    //getter and setter
+    public int getAttackBonus() {
+        return attackBonus;
+    }
+
+    public void setAttackBonus(int attackBonus) {
+        this.attackBonus = attackBonus;
+    }
 }
+
