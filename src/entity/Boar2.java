5e795853d74023ff213c391ec38e3733701c052a
”Jiaxin2727“ <1006889290@qq.com>
2026-06-28 22:56:41 +0800

    Monsters update

---
+++ src/entity/Boar2.java
@@ -0,0 +1,11 @@
+package entity;
+
+public class Boar2 extends Monster2{
+    //构造方法
+    public Boar2() {
+    }
+
+    public Boar2(String name, int attack, Item dropItem) {
+        super(name, attack, dropItem);
+    }
+}
